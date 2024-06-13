b88289feeb6b3c87a974ba5cf0f6e724d5665b49
cosc19111119 <115706969+cosc19111119@users.noreply.github.com>
2024-06-13 21:42:53 +0500

    nav changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css'
 import { Routes, Route,} from "react-router-dom";
 import Home from './pages/home/Home';
 import Navbar from './components/layout/Navbar';
+import NavBar from './components/layout/NavBarText';
 function App() {
   return (
     <>
      <Navbar/>
+     {/* <NavBar/> */}
      <Routes>
      <Route path="/" element={<Home />} />
      </Routes>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,27 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Navbar = () => {
-  const links = [
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const toggleScrolled = () => {
+      setIsScrolled(window.scrollY > 80); 
+    };
+    window.addEventListener("scroll", toggleScrolled);
+    
+    return () => {
+      window.removeEventListener("scroll", toggleScrolled);
+    };
+  }, []);
+  const linksExpanded = [
     "HOME",
     "ABOUT",
-    "SERVICES",
-    "PORTFOLIO",
-    "TEAM",
-    "BLOG",
+    "MENUS",
+    "PRIVATE DINING",
+    "EVENTS & SPECIALS",
+    "GALLERY",
+    "GIFT CARDS",
     "CONTACT",
-    "FAQ",
+    
   ];
+
+  const linksScrolled = [
+    "HOME",
+    "ABOUT",
+    "MENUS",
+    "PRIVATE DINING",
+    "EVENTS & SPECIALS",
+    "GALLERY",
+    "GIFT CARDS",
+    "CONTACT",
+  ];
+
+  const linksToShow = isScrolled ? linksScrolled : linksExpanded;
+
   return (
-    <div className="bg-[#400000] h-[75px] flex items-center justify-center  ">
-    <div className="flex justify-center items-center space-x-4">
-      {links.map((link, index) => (
-        <a key={index} href="#" className="text-[#ffd400] text-[12px]">
-          {link}
-        </a>
-      ))}
-      <div></div>
-    </div>
-    </div>
+    <>
+      <div className={`bg-[#400000]  main-tool-bar   flex items-center justify-center ${isScrolled ? 'main-tool-bar--scrolled ' : ''}`}>
+        <div className="flex justify-center gap-10  ">
+          {linksToShow.map((link, index) => (
+            <a key={index} href="#" className="text-[#ffd400] tracking-[1px] text-[12px]">
+              {link}
+            </a>
+          ))}
+          <div className="z-50 w-44 min-h-10 tracking-[1px] bg-[#e1b728] border-0 rounded text-center text-[13px] text-[#580000] leading-10 fixed top-[17px] right-8 book-table">
+            BOOK A TABLE
+          </div>
+        </div>
+      </div>
+      {/* <div className="border flex z-50 relative border-[#ffd400] border-b-10"></div> */}
+      <div className="h-[1300px]"></div>
+      
+    </>
   );
 };
 
