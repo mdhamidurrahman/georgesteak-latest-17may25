bb99c031924fd9020d6ebc1f94004cde3e9dd74f
cosc19111119 <115706969+cosc19111119@users.noreply.github.com>
2024-06-13 15:44:04 +0500

    some changes

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
+  const links = [
+    "HOME",
+    "ABOUT",
+    "SERVICES",
+    "PORTFOLIO",
+    "TEAM",
+    "BLOG",
+    "CONTACT",
+    "FAQ",
+  ];
   return (
-    <div className='text-[30px] text-blue-600'>Navbar</div>
-  )
-}
+    <div className="bg-[#400000] h-[75px] flex items-center justify-center  ">
+    <div className="flex justify-center items-center space-x-4">
+      {links.map((link, index) => (
+        <a key={index} href="#" className="text-[#ffd400] text-[12px]">
+          {link}
+        </a>
+      ))}
+      <div></div>
+    </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
