b15438f74ba3fc37ee560ae3ee5016fce6fec418
cosc19111119 <115706969+cosc19111119@users.noreply.github.com>
2024-06-13 14:44:52 +0500

    intial templet

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-[30px] text-blue-600'>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-[20px] font-mono text-blue-950'>Home</div>
+  )
+}
+
+export default Home
