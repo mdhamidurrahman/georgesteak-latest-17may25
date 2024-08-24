1cd79ecf9b3b79e07c127d69c37cd62b3fd282ef
tariqbrohi143 <tariqbrohi143@gmail.com>
2024-08-24 22:11:38 +0500

    removed copyrith text

--- src/pages/home/components/Socialicon.tsx
+++ src/pages/home/components/Socialicon.tsx
@@ -1,18 +1,16 @@
-
 export default function Socialicon() {
-    return (
-
-        <section className="social-icons py-20">
-            <div className="social_icon">
-                <i className="fa fa-facebook" aria-hidden="true"></i>
-                <i className="fa fa-instagram" aria-hidden="true"></i>
-            </div>
-            <div className="contetn">
+  return (
+    <section className="social-icons py-20">
+      <div className="social_icon">
+        <i className="fa fa-facebook" aria-hidden="true"></i>
+        <i className="fa fa-instagram" aria-hidden="true"></i>
+      </div>
+      {/* <div className="contetn">
                 <a href="#">ADA COMPLIANCE STATEMENT</a>
                 <p>Copyright 2024. All Rights Reserved
                     <a href="#"> DESIGNED BY NYC RESTAURANT</a>
                 </p>
-            </div>
-        </section>
-    )
+            </div> */}
+    </section>
+  );
 }
