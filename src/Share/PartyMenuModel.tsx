07673b022c1e9eeef2e6bbcc5ac640022784932c
muhammad-asif11 <biz.asif11@gmail.com>
2024-09-18 17:25:23 +0500

    pdf done

--- src/Share/PartyMenuModel.tsx
+++ src/Share/PartyMenuModel.tsx
@@ -12,7 +12,7 @@ const menuItems = [
     description: ["breadcrumbs, oregano, lemon white", "wine sauce"],
   },
   {
-    title: "3H WINE BEER & SODA",
+    title: <span style={{ fontWeight: "700" }}>3H WINE BEER & SODA</span>,
     description: ["$50 per person", "(not including tax & tip)"],
   },
 ];
@@ -27,19 +27,71 @@ const menuItems2 = [
   },
   {
     title: "SOLE OREGANATA",
-    description: ["breadcrumbs, oregano, lemon white", "wine sauce"],
+    description: [
+      "breadcrumbs, oregano, lemon white wine sauce",
+      "NY STRIP STEAKS",
+    ],
   },
   {
-    title: "3H WINE BEER & SODA",
-    description: ["$50 per person", "(not including tax & tip)"],
+    title: (
+      <span style={{ fontWeight: "700", fontSize: "24px" }}>
+        DESSERTS & COFFEE
+      </span>
+    ),
+    description: ["Family Style"],
+  },
+  {
+    title: <span style={{ fontWeight: "700" }}>$54 per person</span>,
+    description: ["(not including tax & tip)"],
   },
 ];
-
+// ===== menuItems 3 ======
+const menuItems3 = [
+  {
+    title: "ORGANIC CHICKEN MARTINI",
+    description: ["parmesan crusted, lemon butter sauce"],
+  },
+  {
+    title: "VEAL SCALLOPINI MARSALA",
+    description: ["mushrooms, marsala wine sauce"],
+  },
+  {
+    title: "SOLE OREGANATA",
+    description: [
+      <div className="mt-[50px]">
+        breadcrumbs, oregano, lemon white wine sauce
+      </div>,
+      <p>GRILLED WILD SALMON</p>,
+      <p>NY STRIP STEAKS</p>,
+      <p>FILET MIGNON</p>,
+      "RIBEYE STEAK",
+    ],
+  },
+];
+// ======= menuItems 4 =======
+const menuItems4 = [
+  {
+    title: "This party menu its",
+    description: [
+      <div className="flex place-items-baseline gap-1">
+        <h2 className="text-lg font-bold">3 hours</h2> food and{" "}
+        <h2 className="text-lg font-bold">Wine Bear & sods</h2>
+      </div>,
+    ],
+  },
+  {
+    title: <h2 className="text-lg font-bold">$45 per person</h2>,
+    description: ["(no tax or tip induced)"],
+  },
+];
 const menuTitle = [
   {
     title: "P A R T Y MENU 1",
     menuItems: menuItems,
+    appetizers: "APPETIZERS",
     familystyle: "choice of",
+    entrees: "ENTREES",
+    choiceof: "choice of",
     heading: "*GEORGE HOUSE SALAD",
     vigitable: "mixed greens, tomatoes, house dressing",
     table: "*MEATBALLS",
@@ -48,21 +100,61 @@ const menuTitle = [
   {
     title: "P A R T Y MENU II",
     menuItems: menuItems2,
+    appetizers: "APPETIZER",
     familystyle: "family style",
+    entrees: "ENTREES",
+    choiceof: "choice of",
     heading: "GEORGE HOUSE SALAD",
     vigitable: "FRESH MOZZARELLA",
     table: "FRIED CALAMARI",
-    Braised: "BAKED CLAMS OREGANATA EGGPLANT ROLLATINI SAUSAGE & PEPPERS",
+    Braised: "BAKED CLAMS OREGANATA",
+    Egg: "EGGPLANT ROLLATINI",
+    Peper: "SAUSAGE & PEPPERS",
     pasta: "PASTA",
     penne: "PENNE ALA VODKA",
   },
   {
     title: "P A R T Y MENU 3",
+    appetizers: "APPETIZER",
     familystyle: "family style",
+    entrees: "ENTREES",
+    choiceof: "choice of",
+    clams: <p style={{ paddingTop: "20px" }}>OSYTERS, CLAMS, SHRIMP </p>,
+    cocktail: <span>cocktail sauce</span>,
+    AHI: "AHI TUNA TARTARE",
     heading: "GEORGE HOUSE SALAD",
     vigitable: "FRESH MOZZARELLA",
     table: "FRIED CALAMARI",
-    Braised: "EGGPLANT ROLLATINI SAUSAGE & PEPPERS",
+    Braised: "EGGPLANT ROLLATINI",
+    Peper: "SAUSAGE & PEPPERS",
+    pasta: "PASTA",
+    penne: "PENNE ALA VODKA",
+    P_menuItems: menuItems3,
+  },
+  // ======= menuItems 4 =======
+  {
+    desserts: "DESSERTS & COFFEE",
+    person: "$85 per person",
+    title: <div className="py-4"></div>,
+    appetizers: <h2 className="py-12">BUFFET STYLE</h2>,
+    clams: (
+      <h1 className="text-2xl font-bold pt-8">*Tray –Mozzarella Caprese</h1>
+    ),
+    cocktail: (
+      <span>
+        Organic Tomato, Red bell peppers, fresh basil, mix olives, Balsamic
+        drizzle
+      </span>
+    ),
+    AHI: <h2 className="text-2xl font-bold">*Tray – Eggplant Rollatini</h2>,
+    heading: "Fresh Mozzarella, ricotta, tomato basil sauce",
+    vigitable: <h2 className="text-2xl font-bold">*Tray –Chicken Martini</h2>,
+    table: "With green peas, lemon white wine sauce ",
+    Braised: <h2 className="text-2xl font-bold">*Tray –Veal Marsala</h2>,
+    Peper: "With mushroom, Marsala wine sauce",
+    pasta: <h2 className="text-2xl font-bold">*Tray- Penne Ala Vodka</h2>,
+    penne: "Penne Pasta with tomato vodka cream sauce",
+    Party_menues: menuItems4,
   },
 ];
 
@@ -88,58 +180,72 @@ const PartyMenuModel = ({ isOpen, onClose }: Props) => {
             <div key={index}>
               <div className="grid place-items-center">
                 <div className="w-full sm:w-[80%] lg:w-[70%] border-4 border-[#400] grid place-items-center p-4 sm:p-2">
-                  <div className="border-2 border-[#400] w-full grid place-items-center gap-4 text-center p-4">
+                  <div className="relative border-2 border-[#400] w-full grid place-items-center gap-4 text-center p-4">
                     <div className="w-3/4 sm:w-2/4 lg:w-1/3">
                       <img
                         src="assets/image/navAnimatedImg.svg"
                         className="w-full"
                       />
                     </div>
+                    {/* ========== Party Menu 4 -> DESSERTS & COFFEE ======== */}
+                    <div className="absolute top-56">
+                      <h2 className="text-2xl sm:text-3xl font-bold">
+                        {item.desserts}{" "}
+                      </h2>
+                      <span>{item.person}</span>
+                    </div>
                     <div className="border-t border-b border-[#400] w-1/3 py-2">
                       {item?.title}
                     </div>
                     <div>
                       <h2 className="text-2xl sm:text-3xl font-bold">
-                        APPETIZERS
+                        {item.appetizers}
                       </h2>
                       <span className="text-lg sm:text-xl">
                         {item.familystyle}
                       </span>
+                      {/* ======= Party Menu 3 ====== */}
+                      <div>
+                        {item.clams}
+                        {item.cocktail}
+                      </div>
                     </div>
                     <div className="pt-4 sm:pt-8 grid gap-2">
-                      <h4>{item.heading} </h4>
+                      <p>{item.AHI} </p>
+                      <h4>{item.heading}</h4>
                       <span>{item.vigitable} </span>
                       <span> {item.table} </span>
                       <span>{item.Braised} </span>
+                      <span>{item.Egg} </span>
+                      <span className="pb-4">{item.Peper} </span>
 
-                      <h2 className="text-2xl sm:text-3xl font-bold">
-                        ENTREES
-                      </h2>
-                      <span className="text-lg sm:text-xl">choice of</span>
-
-                      {index === 1 && (
-                        <div>
+                      {index === 1 || index === 2 || index === 3 ? (
+                        <div className="pb-6">
                           <h2 className="text-2xl sm:text-3xl font-bold">
                             {" "}
                             {item.pasta}
                           </h2>
                           <div className="text-lg sm:text-xl">{item.penne}</div>
                         </div>
+                      ) : (
+                        ""
                       )}
+
+                      <h2 className="text-2xl sm:text-3xl font-bold">
+                        {item.entrees}
+                      </h2>
+                      <span className="text-lg sm:text-xl">
+                        {item.choiceof}
+                      </span>
                     </div>
 
                     <div>
                       {item.menuItems?.map((item, index) => (
-                        <article key={index} className="py-6">
-                          <h2
-                            className={`${
-                              item.title === "3H WINE BEER & SODA"
-                                ? "text-xl font-bold"
-                                : "text-lg"
-                            }`}
-                          >
-                            {item.title}
-                          </h2>
+                        <article
+                          key={index}
+                          className={index === 2 ? "!py-0" : "py-6"}
+                        >
+                          <h2 className="text-lg">{item.title}</h2>
                           {item.description.map((desc, i) => (
                             <span key={i} className="block">
                               {desc}
@@ -147,6 +253,24 @@ const PartyMenuModel = ({ isOpen, onClose }: Props) => {
                           ))}
                         </article>
                       ))}
+                      {
+                        /* ======== Party Menu 3 ======== */
+                        item.P_menuItems?.map((P_item, index) => (
+                          <div key={index}>
+                            <h2>{P_item.title}</h2>
+                            <div>{P_item.description}</div>
+                          </div>
+                        ))
+                      }
+                      {/* ===== List of Menu Items 4 ===== */}
+                      <div className="mt-10">
+                        {item.Party_menues?.map((list, index) => (
+                          <div key={index}>
+                            <h2>{list.title} </h2>
+                            <p>{list.description}</p>
+                          </div>
+                        ))}
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -154,7 +278,7 @@ const PartyMenuModel = ({ isOpen, onClose }: Props) => {
                   className="absolute top-4 z-50 right-2 bg-white p-2 rounded shadow-md"
                   onClick={onClose}
                 >
-                  X
+                  <i className="fa fa-times" aria-hidden="true"></i>
                 </button>
               </div>
             </div>
